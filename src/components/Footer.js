974736ca7a9010b9784017bac9b4e9a01620d8ff
Ishika Maheshwari <ishikamaheshwari8989@gmail.com>
2024-07-15 11:45:00 +0530

    Add navbar and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillPhone } from "react-icons/ai";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Ishika Maheshwari</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year}</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="tel:+918770118463"
+                style={{ color: "white" }}
+                rel="noopener noreferrer"
+              >
+                <AiFillPhone />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
